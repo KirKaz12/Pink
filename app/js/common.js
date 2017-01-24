594e8e6e6e01dae3310f6b1e64f70e66ab392658
KirKaz12 <Nofantasy12@gmail.com>
2017-01-24 22:21:56 +0300

    feedback added

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,31 @@
 var nav = $(".page-header__nav"),
-	navBtn = $(".page-header__btn");
+	navBtn = $(".page-header__btn"),
+	feedback = {
+		arrows: true,
+		dots: false,
+		slidesToShow: 1,
+		slideToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1050,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	};
 $(document).ready(function() {
 	nav.addClass("page-header__nav_closed");
 	navBtn.on("click", function() {
 		nav.toggleClass("page-header__nav_closed page-header__nav_opened");
-	})
+	});
+	$(".feedback__list").slick(feedback);
 });
